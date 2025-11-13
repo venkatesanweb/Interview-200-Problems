23cd344a057231c2c5ef7cbab5a514faaf0a7c14
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2025-11-13 21:30:50 +0530

    Add files via upload
    2 Searching Completed

---
+++ Searching.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Searching{
+
+	public static void main(String [] args){
+	
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=sc.nextInt();
+		}
+		int ele=sc.nextInt();
+		System.out.println(linearSearch(arr,ele));
+		System.out.println(BinearySearch(arr,ele));
+		
+	
+	}
+	private static int linearSearch(int []arr,int ele){
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]==ele){
+				return i;
+			}
+		}
+		return -1;
+	}
+	private static int BinearySearch(int arr[],int ele){
+		int i=0,j=arr.length-1;
+		
+		while(i<=j){
+			int mid=((i+j)/2);
+			if(arr[mid]==ele){
+				return mid;
+			}
+			else if(ele>arr[mid]){
+				i=mid+1;
+			}
+			else if(ele<arr[mid]){
+				j=mid-1;
+			}
+		
+		}
+		return -1;
+	
+	
+	}
+	
+	
+}
