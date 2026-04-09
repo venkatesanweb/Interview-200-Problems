cc175c59f8646bdba5c8e6c1c0ac5dcca41b7a7b
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-04-09 18:04:59 +0530

    Create maxProductDifference.cpp

---
+++ maxProductDifference.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        int n = nums.size();
+        int max1 = INT_MIN;
+        int max2 = INT_MIN;
+        int min1 = INT_MAX;
+        int min2 = INT_MAX;
+
+        for(int num : nums){
+            if(num > max1){
+                max2 = max1;
+                max1 = num;
+            }
+            else if(num > max2){
+                max2 = num;
+            }
+            if(num < min1){
+                min2 = min1;
+                min1 = num;
+            }
+            else if(num < min2){
+                 min2 = num;
+            }
+        }
+        int a = (max1*max2) - (min1*min2);
+        return a;
+    }
+};
